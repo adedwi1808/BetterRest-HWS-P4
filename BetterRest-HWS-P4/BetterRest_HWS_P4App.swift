30917b73cb17680e9685e23c1f9d4751712b4362
adedwi1808 <adedwip1808@gmail.com>
2022-10-12 14:51:43 +0700

    Initial Commit

---
+++ BetterRest-HWS-P4/BetterRest_HWS_P4App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_HWS_P4App.swift
+//  BetterRest-HWS-P4
+//
+//  Created by Ade Dwi Prayitno on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_HWS_P4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
