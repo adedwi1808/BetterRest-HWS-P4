ad4b51492601ffc5478e54bd61f8ea98b88e4204
adedwi1808 <adedwip1808@gmail.com>
2022-10-13 11:31:19 +0700

    WrapUp: Challange from HWS One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on:
    1. Replace each VStack in our form with a Section, where the text view is the title of the section. Do you prefer this layout or the VStack layout? It’s your app – you choose!
    2. Replace the “Number of cups” stepper with a Picker showing the same range of values.
    3. Change the user interface so that it always shows their recommended bedtime using a nice and large font. You should be able to remove the “Calculate” button entirely.

--- BetterRest-HWS-P4/ContentView.swift
+++ BetterRest-HWS-P4/ContentView.swift
@@ -26,33 +26,54 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up ?")
-                    .font(.headline)
-                
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desire amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 2...12, step: 0.25)
+            Form {
+                Section {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(.wheel)
                     
-                Text("Daily coffee intake")
-                    .font(.headline)
+                } header: {
+                    Text("When do you want to wake up ?")
+                        .font(.headline)
+                }
                 
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 2...12, step: 0.25)
+                } header: {
+                    Text("Desire amount of sleep")
+                        .font(.headline)
+                }
+                
+                Section {
+//                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text($0, format: .number)
+                        }
+                    }
+                } header: {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                }
+                
+                Section {
+                    Text(alertMessage)
+                } header: {
+                    Text(alertTitle)
+                        .font(.headline)
+                }
             }
-            .padding()
             .navigationTitle("BetterRest")
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("Ok"){}
-            } message: {
-                Text(alertMessage)
-            }
+//            .alert(alertTitle, isPresented: $showingAlert) {
+//                Button("Ok"){}
+//            } message: {
+//                Text(alertMessage)
+//            }
+        }.onAppear() {
+            calculateBedtime()
         }
     }
     func calculateBedtime() {
